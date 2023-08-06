3062bbd4655a4a363dde3894b3ddf7378d874325
filatkinen <filatkinen@gmail.com>
2023-08-06 16:27:57 +0300

    HA#1 is completed#7

--- internal/server/http/middleware-prometeus.go
+++ internal/server/http/middleware-prometeus.go
@@ -23,20 +23,20 @@ func NewPromData() *promData {
 	pr := promData{
 		totalRequests: prometheus.NewCounterVec(
 			prometheus.CounterOpts{
-				Name: "http_requests_total",
+				Name: "goapp_http_requests_total",
 				Help: "Number of get requests.",
 			},
 			[]string{"path"},
 		),
 		responseStatus: prometheus.NewCounterVec(
 			prometheus.CounterOpts{
-				Name: "response_status",
+				Name: "goapp_response_status",
 				Help: "Status of HTTP response",
 			},
 			[]string{"status"},
 		),
 		httpDuration: promauto.NewHistogramVec(prometheus.HistogramOpts{
-			Name: "http_response_time_seconds",
+			Name: "goapp_http_response_time_seconds",
 			Help: "Duration of HTTP requests.",
 		}, []string{"path"}),
 	}
